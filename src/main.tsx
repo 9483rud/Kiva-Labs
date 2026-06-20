ef7d55738d5a819b4a45de0cd253488ab8d6f668
J2 Rist <131925001+9483rud@users.noreply.github.com>
2026-06-20 14:55:12 -0400

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+// Ensure the root element exists before rendering
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error("Failed to find the root element. Make sure index.html has a <div id='root'></div>");
+}
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
