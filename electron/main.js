fda7d4e254e79df55bfd55de821950b51eb2ef21
J2 Rist <J2rist@gmail.com>
2026-07-08 11:46:54 -0500

    Fix electron install

---
+++ electron/main.js
@@ -0,0 +1,45 @@
+import { app, BrowserWindow } from 'electron';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  const devServerUrl = process.env.VITE_DEV_SERVER_URL;
+  const distIndexPath = path.join(__dirname, '..', 'dist', 'index.html');
+
+  if (devServerUrl) {
+    win.loadURL(devServerUrl);
+  } else if (fs.existsSync(distIndexPath)) {
+    win.loadFile(distIndexPath);
+  } else {
+    win.loadURL('http://localhost:5173');
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
